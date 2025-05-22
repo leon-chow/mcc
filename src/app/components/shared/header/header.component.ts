12b6ae02ccf0b983eeed72a7de8365589f4bf685
leon-chow <leon.chow1@ontariotechu.net>
2025-05-21 22:51:22 -0400

    add mat tool bar, fix styling on navbar

--- src/app/components/shared/header/header.component.ts
+++ src/app/components/shared/header/header.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { INavLink, NAV_LINKS } from '../../../constants';
 import { CommonModule } from '@angular/common';
+import { MatToolbar, MatToolbarRow, MatToolbarModule } from "@angular/material/toolbar";
 
 @Component({
   selector: 'app-header-component',
-  imports: [CommonModule],
+  imports: [CommonModule, MatToolbar, MatToolbarRow, MatToolbarModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css',
 })
